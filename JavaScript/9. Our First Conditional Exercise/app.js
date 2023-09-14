2db4ef0b844d01c237e1930afba14dc1f36de3d0
Syarief Azman <syarief.azman@gmail.com>
2023-09-15 04:15:02 +0800

    Our First Conditional Exercise

---
+++ JavaScript/9. Our First Conditional Exercise/app.js
@@ -0,0 +1,12 @@
+let num = Math.ceil(Math.random() * 10);
+
+//WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓
+if (num % 2 === 0) {
+  console.log("even");
+  console.log(num);
+}
+if (num % 2 !== 0) {
+  console.log("odd");
+  console.log(num);
+}
+//AND THIS LINE ↑↑↑↑↑
