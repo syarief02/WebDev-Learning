748fa77d509b57a2dd832487ea4d5cca5deccf93
Syarief Azman <syarief.azman@gmail.com>
2023-09-15 03:56:33 +0800

    if statement

---
+++ JavaScript/8. If Statement/app.js
@@ -0,0 +1,10 @@
+let random = Math.random();
+
+if (random < 0.5){
+    console.log("YOUR NUMBER IS LESS THAN 0.5");
+    console.log(random);
+}
+if (random > 0.5){
+    console.log("YOUR NUMBER IS GREATER THAN 0.5");
+    console.log(random);
+}
