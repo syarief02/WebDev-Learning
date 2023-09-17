e83438ce2cf39157e655ac11f749ad2aef6813ea
Syarief Azman <syarief.azman@gmail.com>
2023-09-18 04:05:10 +0800

    Lotto Numbers Exercise

---
+++ JavaScript/13. Lotto Numbers Exercise/app.js
@@ -0,0 +1,7 @@
+
+// Lotto Numbers Exercise
+// Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere!
+
+// Define lottoNumbers below:
+let lottoNumbers= [1,2,3,4,5,6];
+console.log(lottoNumbers);
