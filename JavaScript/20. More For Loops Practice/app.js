76f86e49450537da7cd68ba84c48125d10097b40
Syarief Azman <syarief.azman@gmail.com>
2023-09-22 22:38:36 +0800

    More For Loops Practice

---
+++ JavaScript/20. More For Loops Practice/app.js
@@ -0,0 +1,21 @@
+// More For Loops Practice
+// Let's try writing a for loop that counts down, rather than up. Please write a for loop that prints the following numbers (in this order):
+
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+// Write a loop that prints:
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+for (let i = 25; i >= 0; i -= 5){
+    console.log(i)
+}
