900aca5ba13077aad92703609bec00b0a0522687
Syarief Azman <syarief.azman@gmail.com>
2023-09-29 09:59:36 +0800

    finished todo apps

--- JavaScript/23. Todo List Project/app.js
+++ JavaScript/23. Todo List Project/app.js
@@ -1,8 +1,10 @@
 let input = prompt('what would you like to do?');
-let toDoList = [""];
+let toDoList = ["egg", "litter"];
 
-if (!input) { input = prompt('invalid prompt'); }
-while (input.toLowerCase() !== "quit") {
+if (!input) {
+    input = prompt('invalid prompt');
+}
+while (input.toLowerCase() !== "quit" && input.toLowerCase() !== "q") {
     if (input === "new") {
         input = prompt('add Todo');
 
@@ -12,12 +14,17 @@ while (input.toLowerCase() !== "quit") {
     }
     else if (input === "list") {
         console.log("************")
-        for (i = 0; i < input.length; i++) {
-            console.log(`${i}: ${input[i]} `);
-
-            break;
+        for (i = 0; i < toDoList.length; i++) {
+            console.log(`${i}: ${toDoList[i]} `);
         }
         console.log("************")
+    }
+    else if (input === "delete") {
+       let inputNum = prompt("type entry number");
+        if ((inputNum < toDoList.length) && (inputNum >= 0)) {
+            toDoList.splice(inputNum, 1);
+        }
+        else input === prompt("entry does not exist. type list to show entry lists");
     }
     else input = prompt('invalid prompt');
     input = prompt('what would you like to do?');
