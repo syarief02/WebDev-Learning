7ee0b89d1cfa77b616ce8dd8efe20671b2319ac0
Syarief Azman <syarief.azman@gmail.com>
2023-09-30 04:03:24 +0800

    Heart Function Exercise

---
+++ JavaScript/24. Heart Function Exercise/app.js
@@ -0,0 +1,13 @@
+
+// Heart Function Exercise
+// It's time to write our first function!
+
+// Define a function called printHeart that prints out the string "<3"
+
+// Execute your function once
+
+// Write your function here:
+function printHeart() {
+    console.log("<3");
+}
+printHeart();
