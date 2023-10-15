aa78ee267fa8edd3d7a87e819fb059872c46826d
Syarief Azman <syarief.azman@gmail.com>
2023-10-15 21:16:12 +0800

    Arrow Function Exercise

---
+++ JavaScript/37. Arrow Function Exercise/app.js
@@ -0,0 +1,14 @@
+
+// Arrow Function Exercise
+// Write an arrow function expression called greet.  It should accept a single string argument, representing a person's name.  It should return a greeting string as shown below:
+
+// greet("Hagrid") //"Hey Hagrid!" 
+// greet("Luna") //"Hey Luna!"
+// Be sure to use arrow function syntax!
+
+const greet = (str) =>{
+    return `Hey ${str}!`;
+}
+
+console.log(greet("Hagrid"));
+console.log(greet("Luna"));
