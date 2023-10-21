d9f8f18a21772ddc14f5965cf38f7841bee56d52
Syarief Azman <syarief.azman@gmail.com>
2023-10-21 20:07:25 +0800

    getElementById Practice

---
+++ DOM/app.js
@@ -0,0 +1,14 @@
+// getElementById Practice
+// Let's get some practice using getElementById. I've provided a little bit of markup for you (please don't change it!)  Your goal is to write code in app.js to select the following elements using getElementById:
+
+// Select the image element by its id and save it to a variable called image
+
+// Select the h1 by its id and save it to a variable called heading
+
+// Note: You may need to reset the code before attempting this exercise
+
+// Write your code in here:
+
+const image = document.getElementById("unicorn");
+const heading = document.getElementById("mainheading");
+
