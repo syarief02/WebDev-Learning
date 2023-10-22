614c1496d70541cdf8f7e567c8c79c7ffdaeb7e2
Syarief Azman <syarief.azman@gmail.com>
2023-10-22 21:58:13 +0800

    Pickles Exercise

---
+++ DOM/3. Pickles Exercise/app.js
@@ -0,0 +1,12 @@
+// Pickles Exercise
+// I've provided you with some basic markup in index.html
+
+// Please use JavaScript to select the <span> element that currently reads "Delicious"
+
+// Change its text to read "Disgusting" USING JAVASCRIPT. Even if you are a weirdo who likes pickles, please change the text to "Disgusting". 
+
+// Yes, you could cheat and just update the html file directly, but I hope you don't!  The goal here is to practice using JavaScript to manipulate HTML.
+
+// YOUR CODE GOES IN HERE:
+
+document.querySelector("h1 span").textContent = "Disgusting";
