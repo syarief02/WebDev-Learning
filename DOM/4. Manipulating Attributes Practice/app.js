9273453122602a5605b7f426661ca649a41d7f1b
Syarief Azman <syarief.azman@gmail.com>
2023-10-22 22:30:38 +0800

    Manipulating Attributes Practice

---
+++ DOM/4. Manipulating Attributes Practice/app.js
@@ -0,0 +1,13 @@
+// Manipulating Attributes Practice
+// Let's get some practice working with DOM element attributes. I've provided you with some very basic markup.  Please select the image element and:
+
+// change its source to this url: https://devsprouthosting.com/images/chicken.jpg
+
+// change its alt text to be "chicken"
+
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const image = document.querySelector("img");
+
+image.src = "https://devsprouthosting.com/images/chicken.jpg";
+image.alt = "chicken"
