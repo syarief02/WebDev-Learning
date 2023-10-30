1d5e14956ec7215adff97f3dda25fccc32a435f6
Syarief Azman <syarief.azman@gmail.com>
2023-10-31 03:37:38 +0800

    pokemon sprites

---
+++ DOM/9. Pokemon Sprites Demo/app.js
@@ -0,0 +1,16 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png
+
+const container = document.querySelector("#container");
+
+
+for (let i = 1; i <= 1010; i++) {
+    const pokemon = document.createElement("div");
+    pokemon.classList.add("pokemon");
+    const label = document.createElement("span");
+    label.innerText = `#${i}`;
+    const newImg = document.createElement("img");
+    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
