0887af512e9a68e0379804c4ecf0a06d537d6abf
Syarief Azman <syarief.azman@gmail.com>
2023-11-15 04:18:17 +0800

    make some changes in js

--- DOM Events/7. form Events and PreventDefault 2/app.js
+++ DOM Events/7. form Events and PreventDefault 2/app.js
@@ -1,21 +1,27 @@
+/*
+This JavaScript code is primarily designed for a form submission and reset operation. It first selects the form, input field, and list from the HTML document using their respective IDs. 
+
+Upon the form submission, it prevents the default form submission behavior, logs the submission and input value, creates a new list item with the input value, appends it to the list, and then resets the input field.
+
+There is also a reset button functionality that, when clicked, removes all the children of the list, effectively clearing the list.
+*/
 const form = document.querySelector("#shelterForm");
 const input = document.querySelector("#catName");
 const list = document.querySelector("#cats");
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("SUBMITTED!!");
-    console.log(input.value);
     const catName = input.value;
     const newLi = document.createElement("LI");
     newLi.append(catName);
-    console.log(newLi);
-    list.append(newLi);
+    const fragment = document.createDocumentFragment();
+    fragment.append(newLi);
+    list.append(fragment);
     input.value = "";
 });
 
 const reset = document.querySelector("#reset");
+
 reset.addEventListener("click", function () {
-    for (let i = list.children.length - 1; i >= 0; i--) {
-        list.removeChild(list.children[i]);
-    }
-})
+    list.innerHTML = '';
+});
