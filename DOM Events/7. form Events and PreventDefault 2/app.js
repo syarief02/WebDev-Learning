405f4e38b8c6ccf3e08916bbfd8abe34e287df47
Syarief Azman <syarief.azman@gmail.com>
2023-11-15 02:38:52 +0800

    form events and preventDefault 2

--- DOM Events/7. form Events and PreventDefault 2/app.js
+++ DOM Events/7. form Events and PreventDefault 2/app.js
@@ -1,7 +1,21 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log("SUBMITTED!!");
-    console.log("SUBMITTED!!");
-    console.log("SUBMITTED!!");
+    console.log(input.value);
+    const catName = input.value;
+    const newLi = document.createElement("LI");
+    newLi.append(catName);
+    console.log(newLi);
+    list.append(newLi);
+    input.value = "";
 });
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", function () {
+    for (let i = list.children.length - 1; i >= 0; i--) {
+        list.removeChild(list.children[i]);
+    }
+})
