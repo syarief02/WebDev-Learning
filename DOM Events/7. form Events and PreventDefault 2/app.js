862ebecd58f61860c7e2f5f876a3a5008a79e197
Syarief Azman <syarief.azman@gmail.com>
2023-11-15 01:41:42 +0800

    form events and preventDefault

---
+++ DOM Events/7. form Events and PreventDefault 2/app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#shelterForm");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("SUBMITTED!!");
+    console.log("SUBMITTED!!");
+    console.log("SUBMITTED!!");
+});
