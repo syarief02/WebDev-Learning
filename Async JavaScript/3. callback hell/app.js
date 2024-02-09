c6a54e7011a946e7a28b1eb30b2ac049c65fabd2
Syarief Azman <syarief.azman@gmail.com>
2024-02-09 22:36:10 +0800

    timeout callback in separate function

--- Async JavaScript/3. callback hell/app.js
+++ Async JavaScript/3. callback hell/app.js
@@ -1,14 +1,26 @@
-document.body.style.transition = "1s";
+document.body.style.transition = "1s background-color";
 
-setTimeout(function () {
-    document.body.style.backgroundColor = 'red';
+// Nested callback example:
+// setTimeout(function () {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(function () {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(function () {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(function () {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(function () {
+//                     document.body.style.backgroundColor = 'blue';
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+function delayedColorChange(newColor, delay) {
     setTimeout(function () {
-        document.body.style.backgroundColor = 'orange';
-        setTimeout(function () {
-            document.body.style.backgroundColor = 'yellow';
-        }, 1000);
-    }, 1000);
-}, 1000);
+        document.body.style.backgroundColor = newColor;
+    }, delay);
+}
 
-
-
+delayedColorChange('olive', 3000);
