33842f0d7e0290fa023d77e1199e8ded3fbfacec
Syarief Azman <syarief.azman@gmail.com>
2024-02-09 22:22:08 +0800

    callback hell

---
+++ Async JavaScript/3. callback hell/app.js
@@ -0,0 +1,9 @@
+document.body.style.transition = "1s";
+
+setTimeout(function () {
+    document.body.style.backgroundColor = 'red';
+}, 1000);
+
+setTimeout(function () {
+    document.body.style.backgroundColor = 'orange';
+}, 2000);
