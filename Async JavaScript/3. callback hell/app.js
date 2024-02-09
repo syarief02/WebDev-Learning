94cab37b558e1615698496a7555d0121d4d8bc59
Syarief Azman <syarief.azman@gmail.com>
2024-02-09 22:24:28 +0800

    nesting callback

--- Async JavaScript/3. callback hell/app.js
+++ Async JavaScript/3. callback hell/app.js
@@ -2,8 +2,13 @@ document.body.style.transition = "1s";
 
 setTimeout(function () {
     document.body.style.backgroundColor = 'red';
+    setTimeout(function () {
+        document.body.style.backgroundColor = 'orange';
+        setTimeout(function () {
+            document.body.style.backgroundColor = 'yellow';
+        }, 3000);
+    }, 2000);
 }, 1000);
 
-setTimeout(function () {
-    document.body.style.backgroundColor = 'orange';
-}, 2000);
+
+
