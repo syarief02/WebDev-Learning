f8ca7cd9b41d0185747ed692100a2de709e28a9a
Syarief Azman <syarief.azman@gmail.com>
2024-02-11 22:28:00 +0800

    fakerequest function demo

--- Async JavaScript/4. fake request using callback/app.js
+++ Async JavaScript/4. fake request using callback/app.js
@@ -1,3 +1,11 @@
 function fakeRequestCallback(url, success, failure) {
     const delay = 500 + Math.floor(Math.random() * 4500);
+    setTimeout(function () {
+        if (delay > 4000) {
+            failure('connection timeout :(');
+        }
+        else {
+            success('here is your fake data from ${url}');
+        }
+    })
 }
