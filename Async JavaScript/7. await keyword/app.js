16b3552127140d786a174eb7175e65f674375081
Syarief Azman <syarief.azman@gmail.com>
2024-05-10 00:23:54 +0800

    await keyword

---
+++ Async JavaScript/7. await keyword/app.js
@@ -0,0 +1,28 @@
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+async function rainbow() {
+    await delayedColorChange('red', 1000);
+    await delayedColorChange('orange', 1000);
+    await delayedColorChange('yellow', 1000);
+    await delayedColorChange('green', 1000);
+    await delayedColorChange('blue', 1000);
+    await delayedColorChange('indigo', 1000);
+    await delayedColorChange('violet', 1000);
+}
+
+async function printRainbow() {
+    await rainbow();
+    setTimeout(function () {
+        console.log("End of Rainbow");
+    }, 10000);
+
+}
+
+printRainbow();
