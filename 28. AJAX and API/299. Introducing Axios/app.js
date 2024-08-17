6035658f5b5fb2b2a015bdc18da0efbd1caf9bfe
Syarief Azman <syarief.azman@gmail.com>
2024-08-18 02:38:06 +0800

    Introducing Axios

---
+++ 28. AJAX and API/299. Introducing Axios/app.js
@@ -0,0 +1,21 @@
+// axios.get("https://swapi.dev/api/people/1/")
+//     .then((res) => {
+//         console.log("response: ", res);
+//     })
+//     .catch((e) => {
+//         console.log("ERROR! ", e);
+//     });
+
+
+
+const getStarWarsPerson = async (id) => {
+    try {
+        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+        console.log(res.data);
+    }
+    catch (e) {
+        console.log("ERROR!", e);
+    }
+};
+
+getStarWarsPerson(10);
